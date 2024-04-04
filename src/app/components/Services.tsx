db80c8f1751948f0d68160e4c4d9d9ffffa95f1a
Brandon Garcia <grandonleebarcia@gmail.com>
2024-04-04 15:08:26 -0700

    finish services section

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -3,39 +3,64 @@ import Phone from '../assets/phone-svgrepo-com.svg';
 import Sale from '../assets/sale-badge-svgrepo-com.svg';
 import Shop from '../assets/shopping-card-svgrepo-com.svg';
 
-function Circle() {
+const pink = 'bg-pink-700';
+const purple = 'bg-purple-600';
+
+function Circle({ color }: { color: string }) {
   return (
-    <div className=" w-[300px] h-[300px] bg-pink-700 rounded-full opacity-30"></div>
+    <div
+      className={`absolute w-[160px] h-[160px] ${color} rounded-full opacity-30 mt-[-5px]`}
+    ></div>
   );
 }
 
 export default function Services() {
   return (
-    <section className="flex flex-col gap-24 w-full justify-center items-center pt-12">
+    <section className="flex flex-col gap-24 w-full justify-center items-center pt-12 pb-12">
       <h1 className="text-3xl font-bold">Our Services</h1>
-      <div className="flex  w-full justify-center items-center">
-        <div className="grid gap-2 w-60 text-center">
-          <div className="mx-auto">
-            <Circle></Circle>
-            <Image src={Phone} alt="phone" height={100} width={100} />
+      <div className="flex w-2/3 justify-evenly items-center">
+        <div className="grid gap-2 w-1/4  text-center bg-white pt-10 pb-10 px-5 rounded shadow z-20">
+          <div className=" flex justify-center mx-auto w-full relative">
+            <Image
+              src={Phone}
+              alt="phone"
+              height={150}
+              width={150}
+              className="z-10"
+            />
+            <Circle color={pink}></Circle>
           </div>
-          <h1 className="text-2xl">Lorme Ipmsum</h1>
+          <h1 className="text-2xl font-semibold">Lorme Ipmsum</h1>
           <p>Sed ut perspiciatis unde omnis iste natus error</p>
           <p>read more {'->'}</p>
         </div>
-        <div className="grid gap-2 w-60 text-center">
-          <div className="mx-auto">
-            <Image src={Shop} alt="shop" height={100} width={100} />
+        <div className="grid gap-2 w-1/4 text-center bg-white pt-10 pb-10 px-5 rounded shadow">
+          <div className=" flex justify-center mx-auto w-full relative h-[150px]">
+            <Image
+              src={Shop}
+              alt="shop"
+              height={100}
+              width={100}
+              className="z-10"
+            />
+            <Circle color={purple}></Circle>
           </div>
-          <h1 className="text-2xl">Lorme Ipmsum</h1>
+          <h1 className="text-2xl font-semibold">Lorme Ipmsum</h1>
           <p>Sed ut perspiciatis unde omnis iste natus error</p>
           <p>read more {'->'}</p>
         </div>
-        <div className="grid gap-2 w-60 text-center">
-          <div className="mx-auto">
-            <Image src={Sale} alt="sale" height={100} width={100} />
+        <div className="grid gap-2 w-1/4 text-center bg-white pt-10 pb-10 px-5 rounded shadow">
+          <div className=" flex justify-center mx-auto w-full relative h-[150px]">
+            <Image
+              src={Sale}
+              alt="sale"
+              height={120}
+              width={120}
+              className="z-10"
+            />
+            <Circle color={pink}></Circle>
           </div>
-          <h1 className="text-2xl">Lorme Ipmsum</h1>
+          <h1 className="text-2xl font-semibold ">Lorme Ipmsum</h1>
           <p>Sed ut perspiciatis unde omnis iste natus error</p>
           <p>read more {'->'}</p>
         </div>
